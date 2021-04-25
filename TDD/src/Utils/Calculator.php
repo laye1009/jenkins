b4885b11f13720afcb5a4e6476fc062d59fbe976
laye <layekebe98@hotmail.com>
2021-04-25 17:12:46 +0200

    calculteur ajouté

---
+++ TDD/src/Utils/Calculator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Utils;
+
+class Calculator
+{
+    function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    function substract($a, $b)
+    {
+        return $a - $b;
+    }
+}
